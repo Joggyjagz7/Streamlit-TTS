d2e7e334f93c5c7fee2fac98f6ac5bec797808a3
Ejiro Onose <ejiro10000@gmail.com>
2022-06-24 08:45:00 +0400

    file upload component

--- app.py
+++ app.py
@@ -17,6 +17,11 @@ translator = Translator()
 #buff, col, buff2 = st.beta_columns([4,6,4])
 text = st.text_area("Enter text")
 
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+     bytes_data = uploaded_file.read()
+     st.write("filename:", uploaded_file.name)
+     st.write(bytes_data)
 
 in_lang = st.selectbox(
     "Select your input language",
